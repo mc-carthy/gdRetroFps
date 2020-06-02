ed924c5734d53a7f8819b3cd28dec5d30e22741a
Mick McCarthy <mick@mccarthy.tech>
2020-06-02 21:56:08 +0100

    Add health controller

---
+++ src/Characters/HealthController.gd
@@ -0,0 +1,39 @@
+extends Spatial
+class_name HealthController
+
+signal dead
+signal hurt
+signal healed
+signal health_changed
+signal gibbed
+
+export var max_health: int = 100
+export var gib_at: int = -10
+
+var current_health: int = 0
+
+func _ready() -> void:
+	init()
+
+func init() -> void:
+	current_health = max_health
+	emit_signal('health_changed', current_health)
+
+func hurt(damage: int, dir: Vector3) -> void:
+	if current_health <= 0:
+		return
+	current_health -= damage
+	if current_health <= gib_at:
+		emit_signal('gibbed')
+	if current_health <= 0:
+		emit_signal('dead')
+	emit_signal('hurt')
+	emit_signal('healh_changed', current_health)
+
+func heal(amount: int) -> void:
+	if current_health <= 0:
+		return
+	current_health += amount
+	current_health = clamp(current_health, 0, max_health)
+	emit_signal('healed')
+	emit_signal('health_changed', current_health)
--- src/Characters/Player/Player.gd
+++ src/Characters/Player/Player.gd
@@ -4,14 +4,22 @@ export var mouse_sensitivity: float = 0.5
 
 onready var camera: Camera = $Camera
 onready var character_controller: CharacterController = $CharacterController
+onready var health_controller: HealthController = $HealthController
+
+var dead: bool = false
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	character_controller.init(self)
+	health_controller.init()
+	health_controller.connect('dead', self, 'kill')
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed('exit'):
 		get_tree().quit()
+	
+	if dead:
+		return
 	
 	var move_vector: Vector3 = Vector3.ZERO
 	if Input.is_action_pressed('move_forward'):
@@ -33,3 +41,13 @@ func _input(event: InputEvent) -> void:
 		rotation_degrees.y -= mouse_sensitivity * event.relative.x
 		camera.rotation_degrees.x -= mouse_sensitivity * event.relative.y
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
+
+func hurt(damage: int, dir: Vector3) -> void:
+	health_controller.hurt(damage, dir)
+
+func heal(amount: int) -> void:
+	health_controller.heal(amount)
+
+func kill() -> void:
+	dead = true
+	character_controller.freeze()
