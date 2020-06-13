25bbb31c422f118e97e1a975c7e7e426d82768b4
Mick McCarthy <mick@mccarthy.tech>
2020-06-13 08:51:08 +0100

    Add restart function

--- src/Characters/Player/Player.gd
+++ src/Characters/Player/Player.gd
@@ -26,6 +26,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_pressed('exit'):
 		get_tree().quit()
+	if Input.is_action_just_pressed('restart'):
+		get_tree().reload_current_scene()
 	
 	if dead:
 		return
