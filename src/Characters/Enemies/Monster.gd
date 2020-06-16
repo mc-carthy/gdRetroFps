3601950accf44e73b450c42de0ac0c84d313d6fe
Mick McCarthy <mick@mccarthy.tech>
2020-06-16 08:49:31 +0100

    Add enemy player detection logic

--- src/Characters/Enemies/Monster.gd
+++ src/Characters/Enemies/Monster.gd
@@ -3,10 +3,15 @@ extends KinematicBody
 onready var anim_player: AnimationPlayer = $Model/AnimationPlayer
 onready var health_controller: HealthController = $HealthController
 
+export var view_angle: float = 45
+export var view_dist: float = 50
+
 enum STATES { IDLE, CHASE, ATTACK, DEAD }
 var current_state = STATES.IDLE
+var player = null
 
 func _ready() -> void:
+	player = get_tree().get_nodes_in_group('Player')[0]
 	var bone_attachments: Array = $Model/Armature/Skeleton.get_children()
 	for bone_attachment in bone_attachments:
 		for child in bone_attachment.get_children():
@@ -16,6 +21,7 @@ func _ready() -> void:
 	set_state_idle()
 
 func _process(delta: float) -> void:
+	print(can_see_player())
 	match current_state:
 		STATES.IDLE:
 			process_state_idle(delta)
@@ -54,3 +60,26 @@ func process_state_dead(delta: float) -> void:
 
 func hurt(damage: int, direction: Vector3) -> void:
 	health_controller.hurt(damage, direction)
+
+func can_see_player() -> bool:
+	return player_within_view_dist() and player_within_view_cone() and has_los_player()
+
+func player_within_view_dist() -> bool:
+	return global_transform.origin.distance_squared_to(player.global_transform.origin) < pow(view_dist, 2)
+
+func player_within_view_cone() -> bool:
+	var dir_to_player: Vector3 = global_transform.origin.direction_to(player.global_transform.origin)
+	var forward_vector: Vector3 = global_transform.basis.z
+	var angle_to_player: float = rad2deg(forward_vector.angle_to(dir_to_player))
+	return angle_to_player < view_angle
+
+func has_los_player() -> bool:
+	# The monster/player origins are on the floor, raise them to eye level to prevent unintentional collisions
+	var floor_offset = Vector3.UP * 1.8
+	var pos: Vector3 = global_transform.origin + floor_offset
+	var player_pos: Vector3 = player.global_transform.origin + floor_offset
+	var space_state: PhysicsDirectSpaceState = get_world().get_direct_space_state()
+	var result = space_state.intersect_ray(pos, player_pos, [], 1)
+	if result:
+		 return false
+	return true
