71725f383cb6158f4e709b7df725f48a94a56861
Mick McCarthy <mick@mccarthy.tech>
2020-06-17 21:43:05 +0100

    Add fireball

---
+++ src/Weapons/Fireball.gd
@@ -0,0 +1,22 @@
+extends KinematicBody
+
+var speed: float = 20
+var impact_damage: int = 20
+
+func _ready() -> void:
+	hide()
+
+func set_bodies_to_exclude(bodies: Array) -> void:
+	for body in bodies:
+		add_collision_exception_with(body)
+
+func _physics_process(delta: float) -> void:
+	var collision: KinematicCollision = move_and_collide(-global_transform.basis.z * speed * delta)
+	if collision:
+		var collider = collision.collider
+		if collider.has_method('hurt'):
+			collider.hurt(impact_damage, -global_transform.basis.z)
+		$DestroyAfter.start()
+		$SmokeParticles.emitting = true
+		$Graphics.hide()
+		speed = 0
